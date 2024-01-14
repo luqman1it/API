9aa36e22569a63bebd4b1936959073efd22435ce
luqman1it <111694927+luqman1it@users.noreply.github.com>
2024-01-14 19:57:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,36 @@
-function getAllPosts(userId) {
-    let request = new XMLHttpRequest();
-    request.open("GET", `https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-    request.responseType = "json"
-    request.setRequestHeader("Accept", "application/json");
-    request.setRequestHeader("Content-type", "application/json");
-    request.send();
-    request.onload = function () {
-        let posts = request.response;
-        document.querySelector(".right").innerHTML = "";
 
-        if (request.status >= 200 && request.status < 300) {
-            for (post of posts) {
-                let content = `
-                <div class="box">
-                    <p class="title">${post.title}</p>
-                    <p class="body">${post.body}</p>
-               </div>  `
-                document.querySelector(".right").innerHTML += content;
-            }
-
-        } else {
-            alert("error get")
-        }
+async function getAllPosts(userId) {
+    let response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    let posts = await response.json();
+    document.querySelector(".right").innerHTML = "";
+    for (post of posts) {
+        let content = `
+                    <div class="box">
+                        <p class="title">${post.title}</p>
+                        <p class="body">${post.body}</p>
+                   </div>  
+        `
+        document.querySelector(".right").innerHTML += content;
     }
+
 }
-function getAlluser() {
-    let request = new XMLHttpRequest();
-    request.open("GET", `https://jsonplaceholder.typicode.com/users`);
-    request.responseType = "json"
-    request.setRequestHeader("Accept", "application/json");
-    request.setRequestHeader("Content-type", "application/json");
-    request.send();
-    request.onload = function () {
-        let users = request.response;
 
-        if (request.status >= 200 && request.status < 300) {
-
-            for (user of users) {
-                let content = `
+async function getAlluser() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let users = await response.json();
+    for (user of users) {
+        let content = `
                 <div class="name" onClick="userClick(${user.id},this)">
                 <i class="fa-solid fa-circle-user"></i>
                     <h4>${user.name}</h4>
                     <p>${user.email}</p>
-                </div> `
-                document.querySelector(".left").innerHTML += content;
-            }
-
-        } else {
-            alert("error get")
-        }
+                </div> 
+    `;
+        document.querySelector(".left").innerHTML += content;
     }
 }
-getAlluser();
 
+getAlluser();
 function userClick(id, color) {
     getAllPosts(id);
     let colorRed = document.querySelectorAll(".name")
